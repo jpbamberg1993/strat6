12363d017c0f3be278606f038dcdae0db5a0779d
jpbamberg1993@gmail.com <jpbamberg1993@gmailc.om>
2015-10-07 22:20:08 -0400

    heroku image trial fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,9 +1,11 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   
-  config.serve_static_assets = true
-  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
-  config.assets.compile = true
+  # config.serve_static_assets = true
+  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
+  # config.assets.compile = true
+
+  config.assets.precompile += %w( .svg .eot .woff .ttf .png .jpg .gif)
 
   # Code is not reloaded between requests.
   config.cache_classes = true
