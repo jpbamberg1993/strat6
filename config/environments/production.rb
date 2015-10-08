29264831c3c2a0e0bbb747104b22296aea15900d
jpbamberg1993@gmail.com <jpbamberg1993@gmailc.om>
2015-10-07 22:22:59 -0400

    heroku trial fix image syntax and config.assets.compile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
   # config.assets.compile = true
 
-  config.assets.precompile += %w( .svg .eot .woff .ttf .png .jpg .gif)
+  # config.assets.precompile += %w( .svg .eot .woff .ttf .png .jpg .gif)
 
   # Code is not reloaded between requests.
   config.cache_classes = true
@@ -35,7 +35,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
